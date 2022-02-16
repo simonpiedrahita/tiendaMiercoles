9cd84645a6880ea6d44c94913ad858c249872139
203 <203@academicos.cesde.edu.co>
2022-02-16 10:05:07 -0500

    se modifica el controlador

--- js/controladorTienda.js
+++ js/controladorTienda.js
@@ -7,7 +7,26 @@ let descripcionProducto="batimovil a escala 1:1"
 let telefonoVendedor ="3223445432"
 //LETRAS MAYUSCULAS CONSTANTES PARA DENOTAR LA CONVENCION DE LAS CONSTANTES
 const NOMBRE_CLIENTE=" SIMON "
+/*arreglos unidimensionales o vectores sirven para almacenar varios elementos en unasola variable 8 un solo espacio de memoria) */
+let productos=[
+    "batimovil",
+    "camiseta",
+    "gorra",
+    "batiMorral"
+]
+let productos2 = Array(
+    "batimovil",
+    "camiseta",
+    "gorra",
+    "batiMorral"
+)
+
+
 
 //salidas
 console.log("buenas"+NOMBRE_CLIENTE+" \n el producto es: "+nombreProducto+"\n descripcion del producto:"+descripcionProducto+"\n el precio del producto es: "+precioProducto)
 console.log (`su nombre es : ${NOMBRE_CLIENTE}\n su producto es:${nombreProducto} \n descripcion del producto: ${descripcionProducto}`)
+console.log("======================================================")
+    //console.log (productos)
+console.log (productos2)
+console.log (productos2[3])
