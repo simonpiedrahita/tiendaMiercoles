062af3a1047c56be7ff0da131497f05ae7857a9f
203 <203@academicos.cesde.edu.co>
2022-02-16 09:48:40 -0500

    se crean las entradas y saidas del programa usando concatenacion y template literals

---
+++ js/controladorTienda.js
@@ -0,0 +1,13 @@
+//entrada del problema 
+let nombreProducto =" batimovil"
+let precioProducto= 400000
+let pesoProducto =18.5
+let tieneDescuento= true
+let descripcionProducto="batimovil a escala 1:1"
+let telefonoVendedor ="3223445432"
+//LETRAS MAYUSCULAS CONSTANTES PARA DENOTAR LA CONVENCION DE LAS CONSTANTES
+const NOMBRE_CLIENTE=" SIMON "
+
+//salidas
+console.log("buenas"+NOMBRE_CLIENTE+" \n el producto es: "+nombreProducto+"\n descripcion del producto:"+descripcionProducto+"\n el precio del producto es: "+precioProducto)
+console.log (`su nombre es : ${NOMBRE_CLIENTE}\n su producto es:${nombreProducto} \n descripcion del producto: ${descripcionProducto}`)
