125c454ce613129b21b91458c1e4b8fdf68c5a1f
203 <203@academicos.cesde.edu.co>
2022-02-23 10:12:28 -0500

    se crea un js para el llenado de productos de la tienda , se integra ese script a el index , se realiza la actividad de creacion de arreglos y objetos

--- js/controladorTienda.js
+++ js/controladorTienda.js
@@ -1,4 +1,4 @@
-//1.se crea una variable para guardar una etiqueta
+/*//1.se crea una variable para guardar una etiqueta
 let etiquetaTitulo = document.getElementById("titulo");
 let etiquetaNavBar=document.getElementById("navbar");
 //impresion por consola de la etiqueta
@@ -16,4 +16,29 @@ etiquetaImagen.src="img/WorldMap.jpg";
 
 //cambiar el estilo de la etiqueta
 etiquetaTitulo.classList.add("text-center")
-etiquetaTitulo.classList.add("text-danger")
+etiquetaTitulo.classList.add("text-danger")*/
+
+//TIENDA 
+
+let productosNombre=["batimovil","camiseta OnePiece","Mug OnePiece","cartel de recompensa Luffy"];
+let productosPrecio=[350000,35000,15000,10000];
+let productosPromocion=[true, false]
+
+console.log(productosNombre);
+console.log(productosNombre[2]);
+
+//creando objetos con javascript
+/*
+let usuario ={
+    nombre:"simon",
+    apellido:"piedrahita",
+    edad:28,
+    amigos:["gatubela","mr frio","el pingüino"]
+}
+
+console.log(usuario);
+console.log("los amigos son:"+usuario.amigos);
+console.log(usuario.amigos[1]);
+
+*/
+
---
+++ js/llenadoTienda.js
@@ -0,0 +1,23 @@
+//creando un arreglo de objetos 
+let productos =[
+    {
+        nombre:"afiche de recompenza Luffy",
+        precio:10000,
+        descripcion:"cartel de se busca de monkey D luffy con recompenza ",
+        cantidad: 50
+    },
+    {
+        nombre:"camiseta arabasta",
+        precio:35000,
+        descripcion:"camiseta personalizada con diseo alusivo a arabasta ",
+        cantidad: 50
+    },
+    {
+        nombre:"afiche de recompenza Zoro",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50
+    },    
+]
+    //10 productos y 10 imagenes 
+console.log(productos);
