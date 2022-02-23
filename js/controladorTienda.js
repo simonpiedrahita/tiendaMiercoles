aeeb97a39ec5a644a1feca25b8b7ce4c3a824226
203 <203@academicos.cesde.edu.co>
2022-02-23 08:40:14 -0500

    se realiza la modificacion de textos e imagenes utilizando el dom de javascript utilizando textcontent y src , se crea un controlador para manipular las etiquetas

--- js/controladorTienda.js
+++ js/controladorTienda.js
@@ -1,32 +1,14 @@
-//entrada del problema 
-let nombreProducto =" batimovil"
-let precioProducto= 400000
-let pesoProducto =18.5
-let tieneDescuento= true
-let descripcionProducto="batimovil a escala 1:1"
-let telefonoVendedor ="3223445432"
-//LETRAS MAYUSCULAS CONSTANTES PARA DENOTAR LA CONVENCION DE LAS CONSTANTES
-const NOMBRE_CLIENTE=" SIMON "
-/*arreglos unidimensionales o vectores sirven para almacenar varios elementos en unasola variable 8 un solo espacio de memoria) */
-let productos=[
-    "batimovil",
-    "camiseta",
-    "gorra",
-    "batiMorral"
-]
-let productos2 = Array(
-    "batimovil",
-    "camiseta",
-    "gorra",
-    "batiMorral"
-)
+//1.se crea una variable para guardar una etiqueta
+let etiquetaTitulo = document.getElementById("titulo");
+let etiquetaNavBar=document.getElementById("navbar");
+//impresion por consola de la etiqueta
+console.log(etiquetaTitulo);
 
+//2. como moddificar el texo de una etiqueta
+etiquetaTitulo.textContent="el anime mas vendido";
+etiquetaNavBar.textContent="OnePiece";
 
-
-//salidas
-console.log("buenas"+NOMBRE_CLIENTE+" \n el producto es: "+nombreProducto+"\n descripcion del producto:"+descripcionProducto+"\n el precio del producto es: "+precioProducto)
-console.log (`su nombre es : ${NOMBRE_CLIENTE}\n su producto es:${nombreProducto} \n descripcion del producto: ${descripcionProducto}`)
-console.log("======================================================")
-    //console.log (productos)
-console.log (productos2)
-console.log (productos2[3])
+//controlando una imagen 
+//src me permite cambiar el atributo de la ubicacion del archivo sin odificar directamente en el html 
+let etiquetaImagen =document.getElementById("fotoLogo");
+etiquetaImagen.src="img/WorldMap.jpg";
