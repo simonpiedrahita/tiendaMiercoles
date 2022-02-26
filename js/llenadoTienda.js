c8e55725458a5412dd048a4a07dad8dbaebf3577
simonpiedrahita <simonpiedrahita@gmail.com>
2022-02-25 23:12:20 -0500

    agrego imagenes para los productos
    sea grega imagen del cartel de cada uno de los tripulantes ,
    se agregan los objetos de cada uno de los item de los cuales se agrego imagen , queda pendiente la imagen de las camisetas

--- js/llenadoTienda.js
+++ js/llenadoTienda.js
@@ -1,23 +1,101 @@
 //creando un arreglo de objetos 
 let productos =[
     {
-        nombre:"afiche de recompenza Luffy",
+        nombre:"Afiche de recompenza Luffy",
         precio:10000,
         descripcion:"cartel de se busca de monkey D luffy con recompenza ",
-        cantidad: 50
+        cantidad: 50,
+        
     },
     {
-        nombre:"camiseta arabasta",
-        precio:35000,
-        descripcion:"camiseta personalizada con diseo alusivo a arabasta ",
-        cantidad: 50
-    },
-    {
-        nombre:"afiche de recompenza Zoro",
+        nombre:"Afiche de recompenza Zoro",
         precio:10000,
         descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
         cantidad: 50
     },    
+    {
+        nombre:"Afiche de recompenza Sanji",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50
+    },    
+    {
+        nombre:"Afiche de recompenza Usopp",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50
+    },    
+    {
+        nombre:"Afiche de recompenza Chopper",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50
+    },    
+    {
+        nombre:"Afiche de recompenza Brook",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50
+    }, 
+    {
+        nombre:"Afiche de recompenza Nami",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50
+    }, {
+        nombre:"Afiche de recompenza Robin",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50
+    }, 
+    {
+        nombre:"Camiseta de Luffy",
+        precio:10000,
+        descripcion:"cartel de se busca de monkey D luffy con recompenza ",
+        cantidad: 50,
+        
+    },
+    {
+        nombre:"Camiseta de Zoro",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50
+    },    
+    {
+        nombre:"Camiseta de Sanji",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50
+    },    
+    {
+        nombre:"Camiseta de Usopp",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50
+    },    
+    {
+        nombre:"Camiseta de Chopper",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50
+    },    
+    {
+        nombre:"Camiseta de Brook",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50
+    }, 
+    {
+        nombre:"Camiseta de Nami",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50
+    }, {
+        nombre:"Camiseta de Robin",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50
+    } 
 ]
     //10 productos y 10 imagenes 
 console.log(productos);
