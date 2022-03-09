ad7d5277c395c02451ea1a730e0eb806ef9e77bd
206 <206@academicos.cesde.edu.co>
2022-03-09 09:56:11 -0500

    se crean las tarjetas utilizando traversing y se llenan desde js

--- js/llenadoTienda.js
+++ js/llenadoTienda.js
@@ -13,49 +13,63 @@ let productos =[
         precio:10000,
         descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
         cantidad: 50,
-        foto:'img/carteles/cartel_de_Luffy.png'
+        foto:'img/carteles/cartel_de_zoro.jpg'
     },    
     {
         nombre:"Afiche de recompenza Sanji",
         precio:10000,
         descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
         cantidad: 50,
-        foto:'img/carteles/cartel_de_Luffy.png'
+        foto:'img/carteles/cartel_de_sanji.jpg'
     },    
     {
         nombre:"Afiche de recompenza Usopp",
         precio:10000,
         descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
         cantidad: 50,
-        foto:'img/carteles/cartel_de_Luffy.png'
+        foto:'img/carteles/cartel_de_usopp.jpg'
     },    
     {
         nombre:"Afiche de recompenza Chopper",
         precio:10000,
         descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
         cantidad: 50,
-        foto:'img/carteles/cartel_de_Luffy.png'
+        foto:'img/carteles/cartel_de_chopper.jpg'
     },    
     {
         nombre:"Afiche de recompenza Brook",
         precio:10000,
         descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
         cantidad: 50,
-        foto:'img/carteles/cartel_de_Luffy.png'
+        foto:'img/carteles/cartel_de_brook.jpg'
     }, 
     {
         nombre:"Afiche de recompenza Nami",
         precio:10000,
         descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
         cantidad: 50,
-        foto:'img/carteles/cartel_de_Luffy.png'
+        foto:'img/carteles/cartel_de_nami.jpg'
     }, {
         nombre:"Afiche de recompenza Robin",
         precio:10000,
         descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
         cantidad: 50,
-        foto:'img/carteles/cartel_de_Luffy.png'
+        foto:'img/carteles/cartel_de_robin.jpg'
     }, 
+    {
+        nombre:"Afiche de recompenza Franky",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50,
+        foto:'img/carteles/cartel_de_franky.jpg'
+    },{
+        nombre:"Afiche de recompenza Jimbe",
+        precio:10000,
+        descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
+        cantidad: 50,
+        foto:'img/carteles/cartel_de_jimbe.jpg'
+    },  
+    /*
     {
         nombre:"Camiseta de Luffy",
         precio:10000,
@@ -106,5 +120,55 @@ let productos =[
         descripcion:"cartel de se busca de Roronoa Zoro con recompenza ",
         cantidad: 50
     } 
+    */
 ]
+
 console.log(productos);
+
+//1. creo una variable para almacenar la base sobre la cual voy a pintar
+let fila=document.getElementById("fila")
+//for each para pintar la informacion de los productos 
+productos.forEach(function (producto){
+
+    console.log(producto.nombre)
+    console.log(producto.descripcion)
+    console.log(producto.precio)
+    console.log(producto.foto)
+
+//2.pintado de etiquetas
+    //div con la clase col 
+    let columna = document.createElement("div")
+     //agregar clases a las etiquetas
+     columna.classList.add("col")
+    let card = document.createElement("div")
+   
+    //agregar clases a las etiquetas
+    card.classList.add("card")
+    card.classList.add("h-100")
+
+    //img con la clase card-img-top
+    let foto = document.createElement("img")
+    foto.classList.add("card-img-top")
+    foto.src=producto.foto
+
+    let cardContent =document.createElement("div")
+    cardContent.classList.add("card-body")
+    
+    //pendiente crear card text y card footer https://getbootstrap.com/docs/5.1/components/card/
+
+    
+    let cardTittle=document.createElement("h4")
+    cardTittle.classList.add("card-tittle")
+    cardTittle.textContent=producto.nombre
+
+//3. padres e hijos o appenddChild's
+    card.appendChild(cardTittle)
+    card.appendChild(foto)
+    card.appendChild(cardContent)
+    card.appendChild(cardContent)
+    columna.appendChild(card)
+    fila.appendChild(columna)
+
+
+});
+
